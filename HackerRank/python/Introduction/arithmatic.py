9f088284b074af84fea5b2c0763232253f5c0674
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-09 01:06:10 +0530

    Arithmetic operation example

---
+++ HackerRank/python/Introduction/arithmatic.py
@@ -0,0 +1,14 @@
+"""
+    Arithmetic operation example
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
+    print(a//b)
+
